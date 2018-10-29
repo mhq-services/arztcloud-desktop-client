0ddce5c5c1edf9f89f7c1b15eed3db0a15b112b5
Jaco <j.lueken@mhq-services.com>
2018-10-29 19:03:13 +0100

    Fix variable name

--- src/autoUpdater.js
+++ src/autoUpdater.js
@@ -13,14 +13,16 @@ autoUpdater.on('error', (err) => {
 
 if (process.platform === 'darwin') {
   autoUpdater.on('update-available', (event, releaseNotes, releaseName, releaseDate, updateUrl) => {
+    // we currently don't support auto update for macOs, so don't trigger any download
+    event.preventDefault();
+
     let result = dialog.showMessageBox({
       type: 'info',
       title: 'Neue Updates verfügbar.',
       message: 'Downloadseite für aktuelle Version öffnen?',
       buttons: ['Ja', 'Nein']
     });
-    e.preventDefault();
-    // we currently don't support auto update for macOs so don't trigger any download
+    
     if (result === 0) {
       require('electron').shell.openExternal(updateUrl);
       app.quit();
